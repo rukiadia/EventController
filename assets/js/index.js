b6c7cebe2598d36a6d40a02c5f2a472b0fe6e01a
sho yamada <taosoftware.s.yamada@gmail.com>
2014-07-03 17:40:53 +0900

    connpassの検索処理追加
    キーワードを固定で指定(HTML5)で指定し、connpassのイベント情報を取得できるようにした。

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,73 @@
 /**
  * Created on 2014/07/03.
  */
-function searchEvent(){
+function searchEvent(siteName){
 
-  // search zusaar
-  var targetUrl = 'http://www.zusaar.com/api/event/?ymd=20140707&format=jsonp';
+  if (siteName.id === 'zusaar') {
+    console.log('zusaar');
 
-  $.ajax({
-    url: targetUrl,
-    type: 'GET',
-    dataType: 'jsonp',
-    timeout: 10000,
-    success: function(data, jsonp) {
+    // search URL
+    var targetUrl = 'http://www.zusaar.com/api/event/?ymd=20140707&format=jsonp';
 
-      var events = data.event;
-      console.log(events);
+    $.ajax({
+      url: targetUrl,
+      type: 'GET',
+      dataType: 'jsonp',
+      timeout: 10000,
+      success: function(data, jsonp) {
+        console.log('success');
+        $('#result').append('<p>Zusaar 検索成功</p>');
 
-      $.each(events, function(key, value){
-        console.log(key + ':' + events[key]['title']);
-      });
+        var eventInfo = data.event;
 
-      $('#result').append('eventURL： ' + events[0]['event_url'] + '<br>');
-      $('#result').append('eventTitle： ' + events[0]['title']);
+        $.each(eventInfo, function(key, value){
+          $('#result').append('URL： ' + eventInfo[key]['event_url'] + '<br>');
+          $('#result').append('eventTitle： ' + eventInfo[key]['title'] + '<br>');
+          $('#result').append('place： ' + eventInfo[key]['place'] + '<br>');
+          $('#result').append('address： ' + eventInfo[key]['address'] + '<br>');
+          $('#result').append('start： ' + eventInfo[key]['started_at'] + '<br><br>');
+        });
+      },
+      error: function(data) {
+        console.log('error');
+      },
+      complete: function() {
+        console.log('complete');
+      }
+    });
 
-    },
-    error: function(data) {
-      console.log('error');
-    },
-    complete: function() {
-      console.log('complete');
-    }
-  });
+  } else if (siteName.id === 'connpass') {
+    console.log('connpass');
+
+    // search URL
+    var targetUrl = 'http://connpass.com/api/v1/event/?keyword=html5';
+
+    $.ajax({
+      url: targetUrl,
+      type: 'GET',
+      dataType: 'jsonp',
+      timeout: 10000,
+      success: function(data, json) {
+        console.log('success');
+        $('#result').append('<p>connpass 検索成功</p>');
+
+        var eventInfo = data.events;
+
+        $.each(eventInfo, function(key, value){
+          $('#result').append('title: ' + eventInfo[key]['title'] + '<br>');
+          $('#result').append('event_url: ' + eventInfo[key]['event_url'] + '<br>');
+          $('#result').append('place: ' + eventInfo[key]['place'] + '<br>');
+          $('#result').append('start: ' + eventInfo[key]['started_at'] + '<br><br>');
+        });
+      },
+      error: function() {
+        console.log('error');
+      },
+      complete: function() {
+        console.log('complete');
+      }
+    });
+
+  }
+
 }
