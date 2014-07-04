ffe3c3ff50ab7cf90cffe231a66f06ba3f9fd4fe
sho yamada <taosoftware.s.yamada@gmail.com>
2014-07-04 15:10:16 +0900

    任意のキーワードによる検索処理追加
    zusaarのみ、API経由でのキーワード検索が失敗し、データが取得できない状態になっている。

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,21 @@
  */
 function searchEvent(siteName){
 
+  // 検索キーワード取得
+  var keyword = $('#keyword').val();
+  console.log(keyword);
+
   // ローディング画像表示 通信処理終了時に消去
   $('#loading').html("<i class='fa fa-spinner fa-3x fa-spin'></i>");
+
+  // 前回の検索結果を消す
+  $('#result').empty();
 
   if (siteName.id === 'atnd') {
     console.log('atnd');
 
     // search URL
     var atndUrl = 'http://api.atnd.org/events/';
-    var keyword = 'html5';
     var format = 'jsonp';
 
     var targetUrl = atndUrl + '?keyword=' + keyword + '&format=' + format;
@@ -22,15 +28,15 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, jsonp) {
-        $('#result').append('<p>Atnd 検索成功</p>');
+        $('#result').append('<p>Atnd イベント検索結果</p>');
 
         var eventInfo = data.events;
 
         $.each(eventInfo, function(key, value){
           $('#result').append('イベントタイトル: ' + eventInfo[key]['title'] + '<br>');
           $('#result').append('イベントURL: ' + eventInfo[key]['event_url'] + '<br>');
-          $('#result').append('開催日: ' + eventInfo[key]['started_at'] + '<br>');
-          $('#result').append('開催場所: ' + eventInfo[key]['place'] + '<br><br>');
+          $('#result').append('開催場所: ' + eventInfo[key]['place'] + '<br>');
+          $('#result').append('開催日時: ' + eventInfo[key]['started_at'] + '<br><br>');
         });
       },
       error: function() {
@@ -39,7 +45,7 @@ function searchEvent(siteName){
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索完了');
+        $('#loading').append('検索が完了しました。');
       }
     });
 
@@ -47,7 +53,9 @@ function searchEvent(siteName){
     console.log('zusaar');
 
     // search URL
-    var targetUrl = 'http://www.zusaar.com/api/event/?ymd=20140707&format=jsonp';
+    var format = 'jsonp';
+    var targetUrl = 'http://www.zusaar.com/api/event/' + '?keyword=' + keyword + '&format=' + format;
+    console.log(targetUrl);
 
     $.ajax({
       url: targetUrl,
@@ -55,9 +63,10 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, jsonp) {
-        $('#result').append('<p>Zusaar 検索成功</p>');
+        $('#result').append('<p>Zusaar イベント検索結果</p>');
 
         var eventInfo = data.event;
+        console.log(eventInfo);
 
         $.each(eventInfo, function(key, value){
           $('#result').append('URL： ' + eventInfo[key]['event_url'] + '<br>');
@@ -69,10 +78,11 @@ function searchEvent(siteName){
       },
       error: function(data) {
         console.log('error');
+        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索完了');
+        $('#loading').append('検索が完了しました。');
       }
     });
 
@@ -80,7 +90,7 @@ function searchEvent(siteName){
     console.log('connpass');
 
     // search URL
-    var targetUrl = 'http://connpass.com/api/v1/event/?keyword=html5';
+    var targetUrl = 'http://connpass.com/api/v1/event/' + '?keyword=' + keyword;
 
     $.ajax({
       url: targetUrl,
@@ -88,23 +98,24 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, json) {
-        $('#result').append('<p>connpass 検索成功</p>');
+        $('#result').append('<p>connpass イベント検索結果</p>');
 
         var eventInfo = data.events;
 
         $.each(eventInfo, function(key, value){
-          $('#result').append('title: ' + eventInfo[key]['title'] + '<br>');
-          $('#result').append('event_url: ' + eventInfo[key]['event_url'] + '<br>');
-          $('#result').append('place: ' + eventInfo[key]['place'] + '<br>');
-          $('#result').append('start: ' + eventInfo[key]['started_at'] + '<br><br>');
+          $('#result').append('イベントタイトル: ' + eventInfo[key]['title'] + '<br>');
+          $('#result').append('イベントURL: ' + eventInfo[key]['event_url'] + '<br>');
+          $('#result').append('開催場所: ' + eventInfo[key]['place'] + '<br>');
+          $('#result').append('開始日時: ' + eventInfo[key]['started_at'] + '<br><br>');
         });
       },
       error: function() {
         console.log('error');
+        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索完了');
+        $('#loading').append('検索が完了しました。');
       }
     });
 
@@ -112,7 +123,7 @@ function searchEvent(siteName){
     console.log('doorkeeper');
 
     // search URL
-    var targetUrl = 'http://api.doorkeeper.jp/events/?q=html5';
+    var targetUrl = 'http://api.doorkeeper.jp/events/' + '?q=' + keyword;
 
     $.ajax({
       url: targetUrl,
@@ -120,20 +131,25 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, json) {
-        $('#result').append('<p>doorkeeper 検索成功</p>');
+        $('#result').append('<p>doorkeeper イベント検索結果</p>');
 
         var eventInfo = data;
+        console.log(eventInfo);
 
         $.each(eventInfo, function(key, value){
-          $('#result').append('title: ' + eventInfo[key]['event']['title'] + '<br><br>');
+          $('#result').append('イベントタイトル: ' + eventInfo[key]['event']['title'] + '<br>');
+          $('#result').append('イベントURL: ' + eventInfo[key]['event']['public_url'] + '<br>');
+          $('#result').append('開催場所: ' + eventInfo[key]['event']['address'] + '<br>');
+          $('#result').append('開催日時: ' + eventInfo[key]['event']['starts_at'] + '<br><br>');
         });
       },
       error: function() {
         console.log('error');
+        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索完了');
+        $('#loading').append('検索が完了しました。');
       }
     });
   }
