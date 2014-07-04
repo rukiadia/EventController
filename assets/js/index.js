7cf6ae9dc4da138f0ce1c6f806839ba491cc5ff7
sho yamada <taosoftware.s.yamada@gmail.com>
2014-07-04 17:50:44 +0900

    古いイベント情報を出さないように修正(doorkeeperのみ)
    開催日時が過ぎているイベントは検索結果に表示しないように変更した。ISO形式のまま開催日時を表示しないように修正。
    
    ※現状、doorkeeperのみ

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,28 @@ function searchEvent(siteName){
 
   // 検索キーワード取得
   var keyword = $('#keyword').val();
-  console.log(keyword);
+
+  // 検索結果の該当件数
+  var count = 0;
+
+  // 現在時刻を取得(イベント日時との比較のため、ISO形式)
+  var date = new Date();
+  var today = date.toISOString();
+
+  // ISO形式を日本時間にする際のオプション
+  var options = {
+    weekday: "long", year: "numeric", month: "short",
+    day: "numeric", hour: "2-digit", minute: "2-digit"
+  };
 
   // ローディング画像表示 通信処理終了時に消去
   $('#loading').html("<i class='fa fa-spinner fa-3x fa-spin'></i>");
 
-  // 前回の検索結果を消す
+  // 前回の検索結果、メッセージを消す
+  $('#message').empty();
   $('#result').empty();
 
   if (siteName.id === 'atnd') {
-    console.log('atnd');
 
     // search URL
     var atndUrl = 'http://api.atnd.org/events/';
@@ -28,11 +40,12 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, jsonp) {
-        $('#result').append('<p>Atnd イベント検索結果</p>');
+        $('#message').append('<p>Atnd イベント検索結果</p>');
 
         var eventInfo = data.events;
 
-        $.each(eventInfo, function(key, value){
+        $.each(eventInfo, function(key, value) {
+          // TODO 日付が古いイベントは画面に出力しないように
           $('#result').append('イベントタイトル: ' + eventInfo[key]['title'] + '<br>');
           $('#result').append('イベントURL: ' + eventInfo[key]['event_url'] + '<br>');
           $('#result').append('開催場所: ' + eventInfo[key]['place'] + '<br>');
@@ -41,16 +54,15 @@ function searchEvent(siteName){
       },
       error: function() {
         console.log('error');
-        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
+        $('#message').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索が完了しました。');
+        $('#message').append('検索が完了しました。');
       }
     });
 
   } else if (siteName.id === 'zusaar') {
-    console.log('zusaar');
 
     // search URL
     var format = 'jsonp';
@@ -63,12 +75,13 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, jsonp) {
-        $('#result').append('<p>Zusaar イベント検索結果</p>');
+        $('#message').append('<p>Zusaar イベント検索結果</p>');
 
         var eventInfo = data.event;
-        console.log(eventInfo);
 
-        $.each(eventInfo, function(key, value){
+        $.each(eventInfo, function(key, value) {
+
+          // TODO 日付が古いイベントは画面に出力しないように
           $('#result').append('URL： ' + eventInfo[key]['event_url'] + '<br>');
           $('#result').append('eventTitle： ' + eventInfo[key]['title'] + '<br>');
           $('#result').append('place： ' + eventInfo[key]['place'] + '<br>');
@@ -78,16 +91,15 @@ function searchEvent(siteName){
       },
       error: function(data) {
         console.log('error');
-        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
+        $('#message').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索が完了しました。');
+        $('#message').append('検索が完了しました。');
       }
     });
 
   } else if (siteName.id === 'connpass') {
-    console.log('connpass');
 
     // search URL
     var targetUrl = 'http://connpass.com/api/v1/event/' + '?keyword=' + keyword;
@@ -98,11 +110,12 @@ function searchEvent(siteName){
       dataType: 'jsonp',
       timeout: 10000,
       success: function(data, json) {
-        $('#result').append('<p>connpass イベント検索結果</p>');
+        $('#message').append('<p>connpass イベント検索結果</p>');
 
         var eventInfo = data.events;
 
         $.each(eventInfo, function(key, value){
+          // TODO 日付が古いイベントは画面に出力しないように
           $('#result').append('イベントタイトル: ' + eventInfo[key]['title'] + '<br>');
           $('#result').append('イベントURL: ' + eventInfo[key]['event_url'] + '<br>');
           $('#result').append('開催場所: ' + eventInfo[key]['place'] + '<br>');
@@ -111,16 +124,15 @@ function searchEvent(siteName){
       },
       error: function() {
         console.log('error');
-        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
+        $('#message').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索が完了しました。');
+        $('#message').append('検索が完了しました。');
       }
     });
 
   } else if (siteName.id === 'doorkeeper') {
-    console.log('doorkeeper');
 
     // search URL
     var targetUrl = 'http://api.doorkeeper.jp/events/' + '?q=' + keyword;
@@ -130,28 +142,49 @@ function searchEvent(siteName){
       type: 'GET',
       dataType: 'jsonp',
       timeout: 10000,
-      success: function(data, json) {
-        $('#result').append('<p>doorkeeper イベント検索結果</p>');
+      success: function(data, jsonp) {
+        $('#message').append('<p>doorkeeper イベント検索結果</p>');
 
         var eventInfo = data;
-        console.log(eventInfo);
 
-        $.each(eventInfo, function(key, value){
-          $('#result').append('イベントタイトル: ' + eventInfo[key]['event']['title'] + '<br>');
-          $('#result').append('イベントURL: ' + eventInfo[key]['event']['public_url'] + '<br>');
-          $('#result').append('開催場所: ' + eventInfo[key]['event']['address'] + '<br>');
-          $('#result').append('開催日時: ' + eventInfo[key]['event']['starts_at'] + '<br><br>');
+        $.each(eventInfo, function(key, value) {
+
+          var eventTime = new Date(eventInfo[key]['event']['starts_at']);
+
+          if (today > eventInfo[key]['event']['starts_at']) {
+            // 既に終了したイベントは画面に出力しない
+            return true;
+          } else {
+            $('#result').append('イベントタイトル: ' + eventInfo[key]['event']['title'] + '<br>');
+            $('#result').append('イベントURL: ' + eventInfo[key]['event']['public_url'] + '<br>');
+            $('#result').append('開催場所: ' + eventInfo[key]['event']['address'] + '<br>');
+            $('#result').append('開催日時: ' + eventTime.toLocaleDateString('ja-JP', options) + '<br><br>');
+
+            count += 1;
+          }
         });
       },
       error: function() {
-        console.log('error');
-        $('#result').append('検索エラーが発生しました。もう一度お試しください。');
+        $('#message').append('検索エラーが発生しました。もう一度お試しください。');
       },
       complete: function() {
         $('#loading').empty();
-        $('#loading').append('検索が完了しました。');
+        $('#message').append('検索が完了しました。');
+
+        // 表示する内容が無かった場合の処理
+        if ( $('#result *').length === 0 ){
+          $('#message').append('該当項目はありませんでした。');
+        } else {
+          $('#message').append('該当項目は' + count + '件です。');
+        }
       }
     });
   }
+}
+
+/* 古いイベント情報を表示しないようにする */
+function removeOldEvent(eventDate) {
+
+  console.log(typeof (eventDate) );
 
 }
